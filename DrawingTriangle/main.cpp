74627c63916d28863c14792f51e3980ae6e42f8e
james <jamessimeao@outlook.com>
2025-12-13 16:54:18 -0300

    Initial commit

---
+++ DrawingTriangle/main.cpp
@@ -0,0 +1,63 @@
+//#include <vulkan/vulkan.h>
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+class HelloTriangleApplication
+{
+public:
+    void run()
+    {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    void initWindow()
+    {
+        glfwInit();
+
+        // Don't create context for OpenGL, since we are using Vulkan
+        glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+        // Disable window resizing
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+    }
+
+    void initVulkan()
+    {
+
+    }
+
+    void mainLoop()
+    {
+
+    }
+
+    void cleanup()
+    {
+
+    }
+};
+
+int main()
+{
+    HelloTriangleApplication app;
+
+    try
+    {
+        app.run();
+    }
+    catch (const std::exception& e)
+    {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
