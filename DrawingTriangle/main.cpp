b0bcb900233f78346afa6a14e46bb6d6c3ca6f63
james <jamessimeao@outlook.com>
2025-12-13 17:55:15 -0300

    Create vulkan instance

--- DrawingTriangle/main.cpp
+++ DrawingTriangle/main.cpp
@@ -15,7 +15,8 @@ private:
     const uint32_t WIDTH {800};
     const uint32_t HEIGHT {600};
 
-
+    // Vulkan instance
+    VkInstance vkInstance;
 
 public:
     void run()
@@ -42,7 +43,36 @@ private:
 
     void initVulkan()
     {
+        createVkInstance();
+    }
 
+    void createVkInstance()
+    {
+        VkApplicationInfo appInfo {};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello Triangle";
+        appInfo.applicationVersion = VK_MAKE_VERSION(1,0,0);
+        appInfo.pEngineName = "No engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1,0,0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        VkInstanceCreateInfo createInfo {};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        uint32_t glfwExtensionCount {0};
+        const char ** glfwExtensions;
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        VkResult result = vkCreateInstance(&createInfo, nullptr, &vkInstance);
+        if(result != VK_SUCCESS)
+        {
+            std::cout << "Failed to create Vulkan instance" << std::endl;
+        }
     }
 
     void mainLoop()
