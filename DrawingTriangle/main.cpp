76080a29415cfae40643ac0e645732ae5f46bb98
james <jamessimeao@outlook.com>
2025-12-30 18:33:56 -0300

    include stb_image.h

--- DrawingTriangle/main.cpp
+++ DrawingTriangle/main.cpp
@@ -17,6 +17,8 @@
 #include <fstream>
 #include <array>
 #include <chrono>
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb/stb_image.h"
 
 // Validation layers
 const std::vector<const char*> validationLayers = {"VK_LAYER_KHRONOS_validation"};
