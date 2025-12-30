ad0c9993a77e75fbbd815b2f668215ed8dfd9508
james <jamessimeao@outlook.com>
2025-12-30 03:05:34 -0300

    Create struct for model view projection, to be used by Vulkan.

--- DrawingTriangle/main.cpp
+++ DrawingTriangle/main.cpp
@@ -96,6 +96,13 @@ const std::vector<uint16_t> vertexIndices
     0, 1, 2, 2, 3, 0
 };
 
+struct UniformBufferObject
+{
+    glm::mat4 model;
+    glm::mat4 view;
+    glm::mat4 proj;
+};
+
 class HelloTriangleApplication
 {
 private:
