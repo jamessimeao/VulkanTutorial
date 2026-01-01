250a4a03bd66030d4b1bde65b282c6760038cd45
james <jamessimeao@outlook.com>
2026-01-01 15:38:09 -0300

    Add texture coordinates to Vertex struct.

--- DrawingTriangle/main.cpp
+++ DrawingTriangle/main.cpp
@@ -59,6 +59,7 @@ struct Vertex
 {
     glm::vec2 pos;
     glm::vec3 color;
+    glm::vec2 textureCoord;
 
     static VkVertexInputBindingDescription getBindingDescription()
     {
@@ -70,9 +71,9 @@ struct Vertex
         return bindingDescription;
     }
 
-    static std::array<VkVertexInputAttributeDescription, 2> getAttributeDescriptions()
+    static std::array<VkVertexInputAttributeDescription, 3> getAttributeDescriptions()
     {
-        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions {};
+        std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions {};
 
         attributeDescriptions[0].binding = 0;
         attributeDescriptions[0].location = 0;
@@ -83,6 +84,11 @@ struct Vertex
         attributeDescriptions[1].location = 1;
         attributeDescriptions[1].format = VK_FORMAT_R32G32B32_SFLOAT;
         attributeDescriptions[1].offset = offsetof(Vertex, color);
+
+        attributeDescriptions[2].binding = 0;
+        attributeDescriptions[2].location = 2;
+        attributeDescriptions[2].format = VK_FORMAT_R32G32B32_SFLOAT;
+        attributeDescriptions[2].offset = offsetof(Vertex, textureCoord);
 
         return attributeDescriptions;
     }
@@ -951,7 +957,7 @@ private:
 
         // Set up thegraphics pipeline to accept vertex data from Vertex struct
         VkVertexInputBindingDescription bindingDescription {Vertex::getBindingDescription()};
-        std::array<VkVertexInputAttributeDescription, 2> attributeDescriptions {Vertex::getAttributeDescriptions()};
+        std::array<VkVertexInputAttributeDescription, 3> attributeDescriptions {Vertex::getAttributeDescriptions()};
 
         VkPipelineVertexInputStateCreateInfo vertexInputStateCreateInfo {};
         vertexInputStateCreateInfo.sType = VK_STRUCTURE_TYPE_PIPELINE_VERTEX_INPUT_STATE_CREATE_INFO;
