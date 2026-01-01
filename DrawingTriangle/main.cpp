a00d0922bdd9b3bc18aecd488ba12ca3d8b9a8e4
james <jamessimeao@outlook.com>
2026-01-01 19:40:26 -0300

    To create method to check if select format for depth buffer has stencil component.

--- DrawingTriangle/main.cpp
+++ DrawingTriangle/main.cpp
@@ -1823,6 +1823,11 @@ private:
 
         throw std::runtime_error("Failed to find supported format.");
     }
+    
+    bool hasStencilComponent(VkFormat format)
+    {
+        return format == VK_FORMAT_D32_SFLOAT_S8_UINT || format == VK_FORMAT_D24_UNORM_S8_UINT;
+    }
 
     VkFormat findDepthFormat()
     {
